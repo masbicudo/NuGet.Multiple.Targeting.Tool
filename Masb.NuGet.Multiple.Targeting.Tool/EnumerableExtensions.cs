3b9019dfdcf7fe19c60e46ee2178583998339fca
masbicudo <masbicudo@gmail.com>
2015-02-13 12:29:34 -0200

    Enumerable extension to work with Task<T> enumerations.

--- Masb.NuGet.Multiple.Targeting.Tool/EnumerableExtensions.cs
+++ Masb.NuGet.Multiple.Targeting.Tool/EnumerableExtensions.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Masb.NuGet.Multiple.Targeting.Tool
 {
@@ -18,6 +19,22 @@ namespace Masb.NuGet.Multiple.Targeting.Tool
             return enumerable
                 .GroupBy(func)
                 .Select(aggregator);
+        }
+
+        /// <summary>
+        /// Selects a value from the result of a task upon it's completion.
+        /// The return will also be an enumeration of tasks of the new selected type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="enumerable"></param>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        public static IEnumerable<Task<TValue>> ThenSelect<T, TValue>(
+            this IEnumerable<Task<T>> enumerable,
+            Func<T, TValue> func)
+        {
+            return enumerable.Select(task => task.ContinueWith(t => func(t.Result)));
         }
     }
 }
