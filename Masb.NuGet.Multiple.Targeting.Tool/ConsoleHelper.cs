2cbcb74f65e605ee144cf4f8a7dbd0dde18b88d2
masbicudo <masbicudo@gmail.com>
2015-02-15 17:20:26 -0200

    Console output with locks (to avoid things text wrong color)

--- Masb.NuGet.Multiple.Targeting.Tool/ConsoleHelper.cs
+++ Masb.NuGet.Multiple.Targeting.Tool/ConsoleHelper.cs
@@ -4,46 +4,63 @@ namespace Masb.NuGet.Multiple.Targeting.Tool
 {
     class ConsoleHelper
     {
+        private static object locker = new object();
+
         public static void Write(string message, ConsoleColor color, int indent)
         {
-            var oldColor = Console.ForegroundColor;
-            Console.ForegroundColor = color;
-            if (IsActive)
-                Console.Write(new string(' ', indent * 4) + message);
-            Console.ForegroundColor = oldColor;
+            lock (locker)
+            {
+                var oldColor = Console.ForegroundColor;
+                Console.ForegroundColor = color;
+                if (IsActive)
+                    Console.Write(new string(' ', indent * 4) + message);
+                Console.ForegroundColor = oldColor;
+            }
         }
 
         public static void Write(string message, ConsoleColor color)
         {
-            var oldColor = Console.ForegroundColor;
-            Console.ForegroundColor = color;
-            if (IsActive)
-                Console.Write(message);
-            Console.ForegroundColor = oldColor;
+            lock (locker)
+            {
+                var oldColor = Console.ForegroundColor;
+                Console.ForegroundColor = color;
+                if (IsActive)
+                    Console.Write(message);
+                Console.ForegroundColor = oldColor;
+            }
         }
 
         public static void WriteLine(string message, ConsoleColor color, int indent)
         {
-            var oldColor = Console.ForegroundColor;
-            Console.ForegroundColor = color;
-            if (IsActive)
-                Console.WriteLine(new string(' ', indent * 4) + message);
-            Console.ForegroundColor = oldColor;
+            lock (locker)
+            {
+                var oldColor = Console.ForegroundColor;
+                Console.ForegroundColor = color;
+                if (IsActive)
+                    Console.WriteLine(new string(' ', indent * 4) + message);
+                Console.ForegroundColor = oldColor;
+            }
         }
 
         public static void WriteLine(string message, ConsoleColor color)
         {
-            var oldColor = Console.ForegroundColor;
-            Console.ForegroundColor = color;
-            if (IsActive)
-                Console.WriteLine(message);
-            Console.ForegroundColor = oldColor;
+            lock (locker)
+            {
+                var oldColor = Console.ForegroundColor;
+                Console.ForegroundColor = color;
+                if (IsActive)
+                    Console.WriteLine(message);
+                Console.ForegroundColor = oldColor;
+            }
         }
 
         public static void WriteLine()
         {
-            if (IsActive)
-                Console.WriteLine();
+            lock (locker)
+            {
+                if (IsActive)
+                    Console.WriteLine();
+            }
         }
 
         public static bool IsActive { get; set; }
